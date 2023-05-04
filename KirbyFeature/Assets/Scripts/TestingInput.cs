ba66001c7b16676f107e7de320028c8a3838536b
elmerdeleon98 <elmerdeleon98@gmail.com>
2023-05-04 14:40:11 -0700

    Destiny's House Work
    Finished kirby movement, created terrain, added MATS, created 1st enemy(will need to code the movement)

--- KirbyFeature/Assets/Scripts/TestingInput.cs
+++ KirbyFeature/Assets/Scripts/TestingInput.cs
@@ -7,6 +7,8 @@ public class TestingInput : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private PlayerInputActions playerInputActions;
+    public int jumpCounter = 0;
+    public bool isGrounded;
 
     private void Awake()
     {
@@ -21,17 +23,21 @@ public class TestingInput : MonoBehaviour
     }
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && jumpCounter<5)
         {
             rigidBody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
             Debug.Log("Jump!:" + context.phase);
+            jumpCounter++;
         }
     }
-    /*public void OnMove(InputAction.CallbackContext context)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        Vector2 moveVec = context.ReadValue<Vector2>();
-        rigidBody.AddForce(new Vector3(moveVec.x,0,moveVec.y) * 5f, ForceMode.Force);
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+            jumpCounter = 0;
+        }
     }
-    */
-   
+
 }
